249142b9c8588621675d3e14567b5da3be659f3a
rafaelsantiagosilva <rafael.santiago.silva.1405@gmail.com>
2024-05-29 20:48:43 -0300

    Adicionando pontuação e ordem correta a ser clicada

--- source/scripts/javascript/quiz/4.js
+++ source/scripts/javascript/quiz/4.js
@@ -4,13 +4,23 @@ const avisoResposta = document.getElementById('aviso_resposta');
 const CLASSES_RESPOSTA = ['acertou', 'intermediario', 'errou'];
 const areasClicaveis = document.querySelectorAll('map > area');
 const listaAreasClicadas = document.getElementById('lista__ordem_organela');
+const ordemCorreta = ['A', 'H', 'D', 'G', 'C', 'E', 'B', 'F'];
+let ordemClicksUsuario = [];
+let respostaCertas = 0;
+let pontuacao = 0;
 const areaRespostaCorreta = document.getElementsByClassName('resposta')[0];
+const iconeResposta = document.getElementById('icone__aviso_resposta');
 let respostaRevelada = false;
 areasClicaveis.forEach((area) => {
     area === null || area === void 0 ? void 0 : area.addEventListener('click', () => {
         var _a;
         let identificacaoOrganela = (_a = area.classList[0]) !== null && _a !== void 0 ? _a : area.id;
         const nomeDaOrganela = nomeOrganela(identificacaoOrganela);
+        const letraDaOrganela = letraOrganela(nomeDaOrganela);
+        if (ordemClicksUsuario.indexOf(letraDaOrganela) === -1)
+            ordemClicksUsuario.push(letraDaOrganela);
+        else
+            ordemClicksUsuario = ordemClicksUsuario.filter((letraOrganela) => letraOrganela != letraDaOrganela);
         const itemExistente = encontraOrganelaNaLista(listaAreasClicadas, nomeDaOrganela);
         if (itemExistente) {
             listaAreasClicadas === null || listaAreasClicadas === void 0 ? void 0 : listaAreasClicadas.removeChild(itemExistente);
@@ -27,6 +37,9 @@ btnEnviar === null || btnEnviar === void 0 ? void 0 : btnEnviar.addEventListener
         .getElementsByClassName('resposta')[0]
         .classList.remove('invisivel');
     const areaRespostaCorreta = document.getElementsByClassName('resposta_correta')[0];
+    console.log(`Clicks: ${ordemClicksUsuario}\nCorreta: ${ordemCorreta}`);
+    respostaCertas = contarRespostasCertas();
+    verificarResultado();
     if (!respostaRevelada) {
         areaRespostaCorreta === null || areaRespostaCorreta === void 0 ? void 0 : areaRespostaCorreta.appendChild(gerarListaResposta());
         areaRespostaCorreta === null || areaRespostaCorreta === void 0 ? void 0 : areaRespostaCorreta.appendChild(gerarImagemResposta());
@@ -63,6 +76,36 @@ function nomeOrganela(idOrganela) {
     }
     return nome;
 }
+function letraOrganela(nomeOrganela) {
+    let letra = '!ERRO!';
+    switch (nomeOrganela.toLowerCase()) {
+        case 'núcleo':
+            letra = 'A';
+            break;
+        case 'retículo endoplasmático':
+            letra = 'B';
+            break;
+        case 'cloroplasto':
+            letra = 'C';
+            break;
+        case 'lisossomo':
+            letra = 'D';
+            break;
+        case 'mitocôndria':
+            letra = 'E';
+            break;
+        case 'complexo de golgi':
+            letra = 'F';
+            break;
+        case 'vacúolo':
+            letra = 'G';
+            break;
+        case 'parede celular':
+            letra = 'H';
+            break;
+    }
+    return letra;
+}
 function encontraOrganelaNaLista(lista, nomeOrganela) {
     if (lista === null || lista === void 0 ? void 0 : lista.children.length) {
         for (let i = 0; i < lista.children.length; i++) {
@@ -76,7 +119,6 @@ function encontraOrganelaNaLista(lista, nomeOrganela) {
 function gerarListaResposta() {
     const listaResposta = document.createElement('ol');
     listaResposta.setAttribute('id', 'lista__resposta');
-    const ordemCorreta = ['A', 'H', 'D', 'G', 'C', 'E', 'B', 'F'];
     for (let i = 0; i < ordemCorreta.length; i++) {
         const item = document.createElement('li');
         item.innerText = ordemCorreta[i];
@@ -91,5 +133,36 @@ function gerarImagemResposta() {
     imagemResposta.alt =
         'Uma célula eucarionte vegetal com suas organelas, com as respostas';
     return imagemResposta;
+}
+function contarRespostasCertas() {
+    let n = 0;
+    for (let i = 0; i < ordemCorreta.length; i++)
+        if (ordemClicksUsuario[i] == ordemCorreta[i])
+            n++;
+    return n;
+}
+function vitoria() {
+    avisoResposta === null || avisoResposta === void 0 ? void 0 : avisoResposta.classList.add(CLASSES_RESPOSTA[0]);
+    avisoResposta.innerHTML += `Parabéns! Você acertou! (${pontuacao.toFixed(2).replace('.', ',')})`;
+    iconeResposta.setAttribute('class', 'bi bi-check-circle');
+}
+function intermediario() {
+    avisoResposta === null || avisoResposta === void 0 ? void 0 : avisoResposta.classList.add(CLASSES_RESPOSTA[1]);
+    avisoResposta.innerHTML += `Quase lá! Continue estudando! (${pontuacao.toFixed(2).replace('.', ',')})`;
+    iconeResposta.setAttribute('class', 'bi bi-exclamation-triangle');
+}
+function derrota() {
+    avisoResposta === null || avisoResposta === void 0 ? void 0 : avisoResposta.classList.add(CLASSES_RESPOSTA[2]);
+    avisoResposta.innerHTML += `Errou! Melhor estudar mais! (${pontuacao.toFixed(2).replace('.', ',')})`;
+    iconeResposta.setAttribute('class', 'bi bi-x-circle');
+}
+function verificarResultado() {
+    avisoResposta === null || avisoResposta === void 0 ? void 0 : avisoResposta.classList.remove('invisivel');
+    if (pontuacao >= 0.83)
+        vitoria();
+    else if (pontuacao >= 0.4 && pontuacao < 0.83)
+        intermediario();
+    else
+        derrota();
 }
 //# sourceMappingURL=4.js.map
--- source/scripts/typescript/quiz/4.ts
+++ source/scripts/typescript/quiz/4.ts
@@ -1,19 +1,34 @@
 const btnEnviar = document.getElementById('botao_enviar');
-const avisoResposta = document.getElementById('aviso_resposta');
+const avisoResposta = document.getElementById('aviso_resposta')!;
 const CLASSES_RESPOSTA: string[] = ['acertou', 'intermediario', 'errou'];
 
 const areasClicaveis = document.querySelectorAll('map > area');
 const listaAreasClicadas = document.getElementById('lista__ordem_organela');
 
+const ordemCorreta: string[] = ['A', 'H', 'D', 'G', 'C', 'E', 'B', 'F'];
+let ordemClicksUsuario: string[] = [];
+let respostaCertas: number = 0;
+let pontuacao: number = 0;
+
 const areaRespostaCorreta = document.getElementsByClassName('resposta')[0];
-let respostaRevelada = false;
+const iconeResposta = document.getElementById('icone__aviso_resposta')!;
+let respostaRevelada: boolean = false;
 
 areasClicaveis.forEach((area) => {
     area?.addEventListener('click', () => {
-        let identificacaoOrganela = area.classList[0] ?? area.id;
-        const nomeDaOrganela = nomeOrganela(identificacaoOrganela);
+        let identificacaoOrganela: string = area.classList[0] ?? area.id;
 
-        const itemExistente = encontraOrganelaNaLista(
+        const nomeDaOrganela: string = nomeOrganela(identificacaoOrganela);
+        const letraDaOrganela: string = letraOrganela(nomeDaOrganela);
+
+        if (ordemClicksUsuario.indexOf(letraDaOrganela) === -1)
+            ordemClicksUsuario.push(letraDaOrganela);
+        else
+            ordemClicksUsuario = ordemClicksUsuario.filter(
+                (letraOrganela) => letraOrganela != letraDaOrganela
+            );
+
+        const itemExistente: HTMLElement | null = encontraOrganelaNaLista(
             listaAreasClicadas,
             nomeDaOrganela
         );
@@ -34,6 +49,12 @@ btnEnviar?.addEventListener('click', () => {
         .classList.remove('invisivel');
     const areaRespostaCorreta: HTMLElement | any =
         document.getElementsByClassName('resposta_correta')[0];
+
+    console.log(`Clicks: ${ordemClicksUsuario}\nCorreta: ${ordemCorreta}`);
+
+    respostaCertas = contarRespostasCertas();
+
+    verificarResultado();
 
     if (!respostaRevelada) {
         areaRespostaCorreta?.appendChild(gerarListaResposta());
@@ -75,6 +96,39 @@ function nomeOrganela(idOrganela: string): string {
     return nome;
 }
 
+function letraOrganela(nomeOrganela: string): string {
+    let letra: string = '!ERRO!';
+
+    switch (nomeOrganela.toLowerCase()) {
+        case 'núcleo':
+            letra = 'A';
+            break;
+        case 'retículo endoplasmático':
+            letra = 'B';
+            break;
+        case 'cloroplasto':
+            letra = 'C';
+            break;
+        case 'lisossomo':
+            letra = 'D';
+            break;
+        case 'mitocôndria':
+            letra = 'E';
+            break;
+        case 'complexo de golgi':
+            letra = 'F';
+            break;
+        case 'vacúolo':
+            letra = 'G';
+            break;
+        case 'parede celular':
+            letra = 'H';
+            break;
+    }
+
+    return letra;
+}
+
 function encontraOrganelaNaLista(
     lista: HTMLElement | null,
     nomeOrganela: string
@@ -92,7 +146,6 @@ function encontraOrganelaNaLista(
 function gerarListaResposta(): HTMLElement {
     const listaResposta = document.createElement('ol');
     listaResposta.setAttribute('id', 'lista__resposta');
-    const ordemCorreta = ['A', 'H', 'D', 'G', 'C', 'E', 'B', 'F'];
 
     for (let i = 0; i < ordemCorreta.length; i++) {
         const item = document.createElement('li');
@@ -112,4 +165,38 @@ function gerarImagemResposta(): HTMLImageElement {
         'Uma célula eucarionte vegetal com suas organelas, com as respostas';
 
     return imagemResposta;
+}
+
+function contarRespostasCertas(): number {
+    let n: number = 0;
+
+    for (let i = 0; i < ordemCorreta.length; i++)
+        if (ordemClicksUsuario[i] == ordemCorreta[i]) n++;
+
+    return n;
+}
+
+function vitoria(): void {
+    avisoResposta?.classList.add(CLASSES_RESPOSTA[0]);
+    avisoResposta.innerHTML += `Parabéns! Você acertou! (${pontuacao.toFixed(2).replace('.', ',')})`;
+    iconeResposta!.setAttribute('class', 'bi bi-check-circle');
+}
+
+function intermediario(): void {
+    avisoResposta?.classList.add(CLASSES_RESPOSTA[1]);
+    avisoResposta.innerHTML += `Quase lá! Continue estudando! (${pontuacao.toFixed(2).replace('.', ',')})`;
+    iconeResposta!.setAttribute('class', 'bi bi-exclamation-triangle');
+}
+
+function derrota(): void {
+    avisoResposta?.classList.add(CLASSES_RESPOSTA[2]);
+    avisoResposta.innerHTML += `Errou! Melhor estudar mais! (${pontuacao.toFixed(2).replace('.', ',')})`;
+    iconeResposta!.setAttribute('class', 'bi bi-x-circle');
+}
+
+function verificarResultado(): void {
+    avisoResposta?.classList.remove('invisivel');
+    if (pontuacao >= 0.83) vitoria();
+    else if (pontuacao >= 0.4 && pontuacao < 0.83) intermediario();
+    else derrota();
 }
