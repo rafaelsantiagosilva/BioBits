1ef24ef0235c6b95020813be2a3ef80ee2dca41b
rafaelsantiagosilva <rafael.santiago.silva.1405@gmail.com>
2024-05-29 08:57:37 -0300

    Mapeando as organelas

--- source/scripts/javascript/quiz/4.js
+++ source/scripts/javascript/quiz/4.js
@@ -2,9 +2,11 @@
 const btnEnviar = document.getElementById('botao_enviar');
 const avisoResposta = document.getElementById('aviso_resposta');
 const CLASSES_RESPOSTA = ['acertou', 'intermediario', 'errou'];
-let classeAtual = 0;
-btnEnviar === null || btnEnviar === void 0 ? void 0 : btnEnviar.addEventListener('click', () => {
-    avisoResposta === null || avisoResposta === void 0 ? void 0 : avisoResposta.setAttribute('class', `${avisoResposta === null || avisoResposta === void 0 ? void 0 : avisoResposta.classList[0]} ${CLASSES_RESPOSTA[classeAtual]}`);
-    classeAtual = classeAtual == 2 ? 0 : classeAtual + 1;
+const areasClicaveis = document.querySelectorAll('map > area');
+areasClicaveis.forEach((area) => {
+    area === null || area === void 0 ? void 0 : area.addEventListener('click', () => {
+        let msg = area.classList[0] ? area.classList[0] : area.id;
+        alert(msg);
+    });
 });
 //# sourceMappingURL=4.js.map
--- source/scripts/typescript/quiz/4.ts
+++ source/scripts/typescript/quiz/4.ts
@@ -2,3 +2,11 @@ const btnEnviar = document.getElementById('botao_enviar');
 const avisoResposta = document.getElementById('aviso_resposta');
 const CLASSES_RESPOSTA: string[] = ['acertou', 'intermediario', 'errou'];
 
+const areasClicaveis = document.querySelectorAll('map > area');
+
+areasClicaveis.forEach((area) => {
+    area?.addEventListener('click', () => {
+        let msg = area.classList[0] ? area.classList[0] : area.id;
+        alert(msg);
+    });
+});
