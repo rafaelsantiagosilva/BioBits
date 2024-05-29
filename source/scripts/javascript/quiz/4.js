feefaa209d70171c2d7aba5047dc383a212075a3
rafaelsantiagosilva <rafael.santiago.silva.1405@gmail.com>
2024-05-29 10:33:38 -0300

    Adicionando a função de ao clicar em uma organela adicionar seu nome a lista

--- source/scripts/javascript/quiz/4.js
+++ source/scripts/javascript/quiz/4.js
@@ -3,11 +3,61 @@ const btnEnviar = document.getElementById('botao_enviar');
 const avisoResposta = document.getElementById('aviso_resposta');
 const CLASSES_RESPOSTA = ['acertou', 'intermediario', 'errou'];
 const areasClicaveis = document.querySelectorAll('map > area');
+const listaAreasClicadas = document.getElementById('lista__ordem_organela');
 areasClicaveis.forEach((area) => {
     area === null || area === void 0 ? void 0 : area.addEventListener('click', () => {
         var _a;
-        let msg = (_a = area.classList[0]) !== null && _a !== void 0 ? _a : area.id;
-        alert(msg);
+        let identificacaoOrganela = (_a = area.classList[0]) !== null && _a !== void 0 ? _a : area.id;
+        const nomeDaOrganela = nomeOrganela(identificacaoOrganela);
+        const itemExistente = encontraOrganelaNaLista(listaAreasClicadas, nomeDaOrganela);
+        if (itemExistente) {
+            listaAreasClicadas === null || listaAreasClicadas === void 0 ? void 0 : listaAreasClicadas.removeChild(itemExistente);
+        }
+        else {
+            const itemDaLista = document.createElement('li');
+            itemDaLista.innerText = nomeDaOrganela;
+            listaAreasClicadas === null || listaAreasClicadas === void 0 ? void 0 : listaAreasClicadas.appendChild(itemDaLista);
+        }
     });
 });
+function nomeOrganela(idOrganela) {
+    let nome = '!ERRO!';
+    switch (idOrganela) {
+        case 'organela1':
+            nome = 'Núcleo';
+            break;
+        case 'organela2':
+            nome = 'Retículo Endoplasmático';
+            break;
+        case 'organela3':
+            nome = 'Cloroplasto';
+            break;
+        case 'organela4':
+            nome = 'Lisossomo';
+            break;
+        case 'organela5':
+            nome = 'Mitocôndria';
+            break;
+        case 'organela6':
+            nome = 'Complexo de Golgi';
+            break;
+        case 'organela7':
+            nome = 'Vacúolo';
+            break;
+        case 'organela8':
+            nome = 'Parede Celular';
+            break;
+    }
+    return nome;
+}
+function encontraOrganelaNaLista(lista, nomeOrganela) {
+    if (lista === null || lista === void 0 ? void 0 : lista.children.length) {
+        for (let i = 0; i < lista.children.length; i++) {
+            if (lista.children[i].innerHTML === nomeOrganela) {
+                return lista.children[i];
+            }
+        }
+    }
+    return null;
+}
 //# sourceMappingURL=4.js.map
--- source/scripts/typescript/quiz/4.ts
+++ source/scripts/typescript/quiz/4.ts
@@ -3,10 +3,68 @@ const avisoResposta = document.getElementById('aviso_resposta');
 const CLASSES_RESPOSTA: string[] = ['acertou', 'intermediario', 'errou'];
 
 const areasClicaveis = document.querySelectorAll('map > area');
+const listaAreasClicadas = document.getElementById('lista__ordem_organela');
 
 areasClicaveis.forEach((area) => {
     area?.addEventListener('click', () => {
-        let msg = area.classList[0] ?? area.id;
-        alert(msg);
+        let identificacaoOrganela = area.classList[0] ?? area.id;
+        const nomeDaOrganela = nomeOrganela(identificacaoOrganela);
+
+        const itemExistente = encontraOrganelaNaLista(listaAreasClicadas, nomeDaOrganela);
+
+        if (itemExistente) {
+            listaAreasClicadas?.removeChild(itemExistente);
+        } else {
+            const itemDaLista = document.createElement('li');
+            itemDaLista.innerText = nomeDaOrganela;
+            listaAreasClicadas?.appendChild(itemDaLista);
+        }
     });
 });
+
+function nomeOrganela(idOrganela: string): string {
+    let nome: string = '!ERRO!';
+
+    switch (idOrganela) {
+        case 'organela1':
+            nome = 'Núcleo';
+            break;
+        case 'organela2':
+            nome = 'Retículo Endoplasmático';
+            break;
+        case 'organela3':
+            nome = 'Cloroplasto';
+            break;
+        case 'organela4':
+            nome = 'Lisossomo';
+            break;
+        case 'organela5':
+            nome = 'Mitocôndria';
+            break;
+        case 'organela6':
+            nome = 'Complexo de Golgi';
+            break;
+        case 'organela7':
+            nome = 'Vacúolo';
+            break;
+        case 'organela8':
+            nome = 'Parede Celular';
+            break;
+    }
+
+    return nome;
+}
+
+function encontraOrganelaNaLista(
+    lista: HTMLElement | null,
+    nomeOrganela: string
+): HTMLLIElement | null {
+    if (lista?.children.length) {
+        for (let i = 0; i < lista.children.length; i++) {
+            if (lista.children[i].innerHTML === nomeOrganela) {
+                return lista.children[i] as HTMLLIElement;
+            }
+        }
+    }
+    return null;
+}
