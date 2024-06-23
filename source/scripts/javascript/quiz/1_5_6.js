1f052a1d3b30d4901f2a0c4d395989c5fe8ee1a5
rafaelsantiagosilva <rafael.santiago.silva.1405@gmail.com>
2024-06-23 19:06:19 -0300

    Arrumando as questõesde múltipla escolha

--- source/scripts/javascript/quiz/1_5_6.js
+++
@@ -1,8 +0,0 @@
-function respostaEscolhida(id){
-    var noclass = document.querySelectorAll(".respostas p")
-    noclass.forEach(function(p) {
-        p.style.color = "black";
-    });
-    var noid = document.getElementById(id)
-    noid.style.color = "blue"
-}
