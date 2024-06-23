0ef296a9fde4c9014f5aba70ec318b9b51d4f362
rafaelsantiagosilva <rafael.santiago.silva.1405@gmail.com>
2024-06-23 17:48:14 -0300

    Terminando a questão 3

---
+++ source/scripts/javascript/quiz/3.js
@@ -0,0 +1,86 @@
+"use strict";
+var _a;
+const imagemTeorias = document.querySelector('#teoria_img > img');
+const listaTeoriasClicadas = document.querySelector('#teorias_clicadas > ol');
+const teorias = document.getElementsByClassName('teoria');
+const clicksUsuario = [];
+const ordemCorretaDasTeorias = [
+    'criacionismo',
+    'abiogenese',
+    'panspermia',
+    'biogenese',
+    'quimica',
+];
+class ProtoImg {
+    constructor(src, alt) {
+        this.src = '../../../public/images/teorias-de-origem-da-vida/';
+        this.alt = '';
+        this.src += src + '.png';
+        this.alt = alt;
+    }
+}
+const imagens = [
+    new ProtoImg('criacionismo', 'Uma mão segurando o mundo'),
+    new ProtoImg('abiogenese', 'Uma camisa branca + trigo = rato'),
+    new ProtoImg('panspermia', 'Um cometa caindo na Terra'),
+    new ProtoImg('biogenese', 'Três frascos: o primeiro com uma carne e fechado; o segundo com uma carne mas com uma rede invés de tampa, atraindo moscas; o terceiro aberto com moscas dentro e larvas na carne'),
+    new ProtoImg('evolucao_quimica', 'Um vulcão soltando diversos gases em um mar de lava. Aparecem moléculas que apóes a eletrização vinda das nuvens viram aminoácidos, proteínas e coacervados.'),
+];
+let indiceImagemAtual = 0;
+(_a = document.getElementById('botao_enviar')) === null || _a === void 0 ? void 0 : _a.addEventListener('click', () => {
+    var _a;
+    let pontuacao = contarAcertosTeorias(clicksUsuario, ordemCorretaDasTeorias) / 5;
+    const GA = new GerenciadorDeAcertos();
+    GA.areaRespostaCorreta.appendChild(gerarRespostaCorreta());
+    GA.verificarResultado(pontuacao);
+    (_a = document.getElementById('botao_enviar')) === null || _a === void 0 ? void 0 : _a.classList.add('invisivel');
+});
+for (let i = 0; i < teorias.length; i++) {
+    teorias[i].addEventListener('click', () => {
+        indiceImagemAtual += indiceImagemAtual < imagens.length - 1 ? 1 : 0;
+        clicksUsuario.push(teorias[i].id);
+        teorias[i].classList.add('riscado');
+        let teoria = teorias[i].innerHTML;
+        let itemDaLista = document.createElement('li');
+        itemDaLista.innerText = teoria;
+        if (!hasChild(listaTeoriasClicadas, itemDaLista))
+            listaTeoriasClicadas.appendChild(itemDaLista);
+        imagemTeorias.setAttribute('src', imagens[indiceImagemAtual].src);
+        imagemTeorias.setAttribute('alt', imagens[indiceImagemAtual].alt);
+    });
+}
+function hasChild(lista, elemento) {
+    const itemsDaLista = lista.children;
+    for (let i = 0; i < itemsDaLista.length; i++) {
+        if (itemsDaLista[i].innerHTML == elemento.innerHTML)
+            return true;
+    }
+    return false;
+}
+function contarAcertosTeorias(clicksUsuario, correta) {
+    let acertos = 0;
+    for (let i = 0; i < clicksUsuario.length; i++) {
+        if (clicksUsuario[i] == correta[i])
+            acertos++;
+    }
+    return acertos;
+}
+function gerarRespostaCorreta() {
+    const respostaCorreta = document.createElement('div');
+    respostaCorreta.setAttribute('class', 'teorias_clicadas');
+    respostaCorreta.appendChild(gerarListaCorretaDasTeorias());
+    return respostaCorreta;
+}
+function gerarListaCorretaDasTeorias() {
+    const listaCorreta = document.createElement('ol');
+    for (let i = 0; i < ordemCorretaDasTeorias.length; i++) {
+        const itemDaLista = document.createElement('li');
+        for (let j = 0; j < teorias.length; j++) {
+            if (ordemCorretaDasTeorias[i] == teorias[j].id)
+                itemDaLista.innerText = teorias[j].textContent;
+        }
+        listaCorreta.appendChild(itemDaLista);
+    }
+    return listaCorreta;
+}
+//# sourceMappingURL=3.js.map
---
+++ source/scripts/typescript/quiz/3.ts
@@ -0,0 +1,116 @@
+const imagemTeorias = document.querySelector('#teoria_img > img')!;
+const listaTeoriasClicadas: HTMLOListElement = document.querySelector(
+    '#teorias_clicadas > ol'
+)!;
+
+const teorias = document.getElementsByClassName('teoria')!;
+const clicksUsuario: string[] = [];
+const ordemCorretaDasTeorias: string[] = [
+    'criacionismo',
+    'abiogenese',
+    'panspermia',
+    'biogenese',
+    'quimica',
+];
+
+class ProtoImg {
+    public src: string = '../../../public/images/teorias-de-origem-da-vida/';
+    public alt: string = '';
+
+    constructor(src: string, alt: string) {
+        this.src += src + '.png';
+        this.alt = alt;
+    }
+}
+
+const imagens: ProtoImg[] = [
+    new ProtoImg('criacionismo', 'Uma mão segurando o mundo'),
+    new ProtoImg('abiogenese', 'Uma camisa branca + trigo = rato'),
+    new ProtoImg('panspermia', 'Um cometa caindo na Terra'),
+    new ProtoImg(
+        'biogenese',
+        'Três frascos: o primeiro com uma carne e fechado; o segundo com uma carne mas com uma rede invés de tampa, atraindo moscas; o terceiro aberto com moscas dentro e larvas na carne'
+    ),
+    new ProtoImg(
+        'evolucao_quimica',
+        'Um vulcão soltando diversos gases em um mar de lava. Aparecem moléculas que apóes a eletrização vinda das nuvens viram aminoácidos, proteínas e coacervados.'
+    ),
+];
+
+let indiceImagemAtual: number = 0;
+
+document.getElementById('botao_enviar')?.addEventListener('click', () => {
+    let pontuacao: number =
+        contarAcertosTeorias(clicksUsuario, ordemCorretaDasTeorias) / 5;
+
+    const GA = new GerenciadorDeAcertos();
+    GA.areaRespostaCorreta.appendChild(gerarRespostaCorreta());
+    GA.verificarResultado(pontuacao);
+    document.getElementById('botao_enviar')?.classList.add('invisivel');
+});
+
+for (let i = 0; i < teorias.length; i++) {
+    teorias[i].addEventListener('click', () => {
+        indiceImagemAtual += indiceImagemAtual < imagens.length - 1 ? 1 : 0;
+
+        clicksUsuario.push(teorias[i].id);
+        teorias[i].classList.add('riscado');
+        let teoria: string = teorias[i].innerHTML;
+
+        let itemDaLista = document.createElement('li');
+        itemDaLista.innerText = teoria;
+
+        if (!hasChild(listaTeoriasClicadas, itemDaLista))
+            listaTeoriasClicadas.appendChild(itemDaLista);
+
+        imagemTeorias.setAttribute('src', imagens[indiceImagemAtual].src);
+        imagemTeorias.setAttribute('alt', imagens[indiceImagemAtual].alt);
+    });
+}
+
+function hasChild(lista: HTMLOListElement, elemento: HTMLLIElement): boolean {
+    const itemsDaLista = lista.children;
+
+    for (let i = 0; i < itemsDaLista.length; i++) {
+        if (itemsDaLista[i].innerHTML == elemento.innerHTML) return true;
+    }
+
+    return false;
+}
+
+function contarAcertosTeorias(
+    clicksUsuario: string[],
+    correta: string[]
+): number {
+    let acertos = 0;
+
+    for (let i = 0; i < clicksUsuario.length; i++) {
+        if (clicksUsuario[i] == correta[i]) acertos++;
+    }
+
+    return acertos;
+}
+
+function gerarRespostaCorreta(): HTMLDivElement {
+    const respostaCorreta = document.createElement('div');
+    respostaCorreta.setAttribute('class', 'teorias_clicadas');
+    respostaCorreta.appendChild(gerarListaCorretaDasTeorias());
+    return respostaCorreta;
+}
+
+function gerarListaCorretaDasTeorias(): HTMLOListElement {
+    const listaCorreta = document.createElement('ol');
+
+    for (let i = 0; i < ordemCorretaDasTeorias.length; i++) {
+        const itemDaLista = document.createElement('li');
+
+        for (let j = 0; j < teorias.length; j++) {
+            if (ordemCorretaDasTeorias[i] == teorias[j].id)
+                itemDaLista.innerText = teorias[j].textContent!;
+        }
+
+        listaCorreta.appendChild(itemDaLista);
+    }
+
+    return listaCorreta;
+}
